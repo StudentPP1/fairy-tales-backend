8e3241a4b55acba668c25b52a23a9884a1d79e69
StudentPP1 <mishamakytonin@gmail.com>
2025-05-05 00:20:59 +0300

    feat: init commit

---
+++ src/main/java/dev/project/bedtimestory/BedtimeStoryApplication.java
@@ -0,0 +1,13 @@
+package dev.project.bedtimestory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BedtimeStoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BedtimeStoryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/project/bedtimestory/BedtimeStoryApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.project.bedtimestory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BedtimeStoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
