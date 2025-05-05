b1dfeafc959bf396d987a49f9d2844da161d607a
StudentPP1 <mishamakytonin@gmail.com>
2025-05-05 03:39:45 +0300

    feat: add migrations

--- src/main/java/dev/project/bedtimestory/entity/User.java
+++ src/main/java/dev/project/bedtimestory/entity/User.java
@@ -14,6 +14,7 @@ import java.util.Set;
 @AllArgsConstructor
 @Setter
 @Getter
+@Table(name = "app_user")
 public class User extends BaseEntity {
     @Column(nullable = false)
     private String name;
